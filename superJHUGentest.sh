632f4904a2612ed426579644e12232b1fc8e9fe9
Heshy Roskes <jroskes1@jhu.edu>
2015-04-17 13:04:13 +0200

    Fix JHUGen errors

--- superJHUGentest.sh
+++ superJHUGentest.sh
@@ -11,6 +11,8 @@ git fetch
 make clean
 git checkout -- .
 git pull
+#modifications to decay JHUGen samples
+sed -i "s/InputLHEFormat = 1/InputLHEFormat = 3/" main.F90
 make
 
 echo "===================="
@@ -18,37 +20,37 @@ echo "=====Generation====="
 echo "===================="
 
 echo "Generating gg --> H --> ZZ..."
-./JHUGen Collider=1 Process=0 VegasNc2=25 OffXVV=011 DecayMode1=9 DecayMode2=9 Unweighted=.true. DataFile=../../$dir/spin0_HZZ
+./JHUGen Collider=1 Process=0 VegasNc2=25 OffXVV=011 DecayMode1=9 DecayMode2=9 Unweighted=1 DataFile=../../$dir/spin0_HZZ
 echo "Generating qqb --> spin1 --> WW..."
-./JHUGen Collider=1 Process=1 VegasNc2=25 OffXVV=011 DecayMode1=11 DecayMode2=11 Unweighted=.true. DataFile=../../$dir/spin1_HWW
+./JHUGen Collider=1 Process=1 VegasNc2=25 OffXVV=011 DecayMode1=11 DecayMode2=11 Unweighted=1 DataFile=../../$dir/spin1_HWW
 echo "Generating gg/qqb --> spin2 --> ZZ..."
-./JHUGen Collider=1 Process=2 VegasNc2=25 PChannel=2 OffXVV=011 DecayMode1=9 DecayMode2=9 Unweighted=.true. DataFile=../../$dir/spin2_HZZ
+./JHUGen Collider=1 Process=2 VegasNc2=25 PChannel=2 OffXVV=011 DecayMode1=9 DecayMode2=9 Unweighted=1 DataFile=../../$dir/spin2_HZZ
 echo "Generating WH..."
-./JHUGen Collider=1 Process=50 VegasNc2=25 OffXVV=011 DecayMode1=11 Unweighted=.true. DataFile=../../$dir/WH
+./JHUGen Collider=1 Process=50 VegasNc2=25 OffXVV=011 DecayMode1=11 Unweighted=1 DataFile=../../$dir/WH
 echo "Decaying WH --> WW..."
 ./JHUGen ReadLHE=../../$dir/WH.lhe DataFile=../../$dir/WH_HWW DecayMode1=11 DecayMode2=11
 echo "Generating ZH..."
-./JHUGen Collider=1 Process=50 VegasNc2=25 OffXVV=011 DecayMode1=9 Unweighted=.true. DataFile=../../$dir/ZH
+./JHUGen Collider=1 Process=50 VegasNc2=25 OffXVV=011 DecayMode1=9 Unweighted=1 DataFile=../../$dir/ZH
 echo "Decaying ZH --> ZZ..."
 ./JHUGen ReadLHE=../../$dir/ZH.lhe DataFile=../../$dir/ZH_HZZ DecayMode1=9 DecayMode2=9
 echo "Generating VBF..."
-./JHUGen Collider=1 Process=60 VegasNc2=25 OffXVV=011 Unweighted=.true. DataFile=../../$dir/VBF
+./JHUGen Collider=1 Process=60 VegasNc2=25 OffXVV=011 Unweighted=1 DataFile=../../$dir/VBF
 echo "Decaying VBF --> WW..."
 ./JHUGen ReadLHE=../../$dir/VBF.lhe DataFile=../../$dir/VBF_HWW DecayMode1=11 DecayMode2=11
 echo "Generating HJJ..."
-./JHUGen Collider=1 Process=61 VegasNc2=25 OffXVV=011 Unweighted=.true. DataFile=../../$dir/HJJ
+./JHUGen Collider=1 Process=61 VegasNc2=25 OffXVV=011 Unweighted=1 DataFile=../../$dir/HJJ
 echo "Decaying HJJ --> ZZ..."
 ./JHUGen ReadLHE=../../$dir/HJJ.lhe DataFile=../../$dir/HJJ_HZZ DecayMode1=9 DecayMode2=9
 echo "Generating HJ..."
-./JHUGen Collider=1 Process=62 VegasNc2=25 OffXVV=011 Unweighted=.true. DataFile=../../$dir/HJ
+./JHUGen Collider=1 Process=62 VegasNc2=25 OffXVV=011 Unweighted=1 DataFile=../../$dir/HJ
 echo "Decaying HJ --> WW..."
 ./JHUGen ReadLHE=../../$dir/HJ.lhe DataFile=../../$dir/HJ_HWW DecayMode1=11 DecayMode2=11
 echo "Generating ttH..."
-./JHUGen Collider=1 Process=80 VegasNc2=25 OffXVV=011 Unweighted=.true. DataFile=../../$dir/ttH
+./JHUGen Collider=1 Process=80 VegasNc2=25 OffXVV=011 Unweighted=1 DataFile=../../$dir/ttH
 echo "Decaying ttH --> ZZ..."
 ./JHUGen ReadLHE=../../$dir/ttH.lhe DataFile=../../$dir/ttH_HZZ DecayMode1=9 DecayMode2=9
 echo "Generating bbH..."
-./JHUGen Collider=1 Process=90 VegasNc2=25 OffXVV=011 Unweighted=.true. DataFile=../../$dir/bbH
+./JHUGen Collider=1 Process=90 VegasNc2=25 OffXVV=011 Unweighted=1 DataFile=../../$dir/bbH
 echo "Decaying bbH --> WW..."
 ./JHUGen ReadLHE=../../$dir/bbH.lhe DataFile=../../$dir/bbH_HWW DecayMode1=11 DecayMode2=11
 
